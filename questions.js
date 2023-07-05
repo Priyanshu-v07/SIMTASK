7bb77ffd9b5c458b57593017e3bb8b079f72d8da
Priyanshu-v07 <138671622+Priyanshu-v07@users.noreply.github.com>
2023-07-05 15:53:31 +0530

    QUIZ APP

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does UML stand for?",
+    answer: "Unified Modelling Language",
+    options: [
+      "Unified Modelling Language",
+      "Unified Meta Language",
+      "Uniform Modelling Language",
+      "Understand Modelling Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "what is the use of hr tag in html?",
+    answer: "Horizontal Line",
+    options: [
+      "None",
+      "break the line ",
+      "shift the text",
+      "Horizontal Line"
+    ]
+  },
+];
